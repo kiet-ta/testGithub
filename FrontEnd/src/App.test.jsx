a320ad79ced35320bcf628affe195265f787bac0
kiet-ta <trananhkiet21082005@gmail.com>
2026-09-17 14:53:35 +0700

    free

--- FrontEnd/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-describe('App component', () => {
-  it('renders Vite + React text', () => {
-    render(<App />)
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
-  })
-})
